eae1d36f2206fca848ea1ea56cf391ef05e8b291
malikabakhtiyorova <malikabakhtiyorova@yandex.ru>
2020-11-10 13:12:30 +0500

    Fix logic issues

--- fahrenheit.js
+++ fahrenheit.js
@@ -7,43 +7,19 @@ fahrenheitForm.addEventListener("submit", function (evt) {
   var fahrenheit = document.querySelector("#fahrenheit");
   fahrenheit.value = parseFloat(formula);
 
-
+  if (isNaN(celcius)) {
+    alert("Raqam kiriting");
+    fahrenheit.classList.remove("bg-primary", "bg-danger");
+    return;
+  }
 
   if (formula >= 80) {
-    fahrenheit.classList.remove("bg-primary", "bg-danger");
+    fahrenheit.classList.remove("bg-primary");
     fahrenheit.classList.add("bg-danger");
   } else if (formula < 80) {
-    fahrenheit.classList.remove("bg-danger", "bg-danger");
+    fahrenheit.classList.remove("bg-danger");
     fahrenheit.classList.add("bg-primary");
   }
 })
 
 
-var gameForm = document.querySelector(".gameForm");
-
-gameForm.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-
-  var number = parseInt(gameForm.querySelector("#number").value.trim(), 10)
-  var result = gameForm.querySelector("#result");
-  // var fizzBuzz = fizzBuzz.textContent = "fizzbuzz";
-  // var buzz = buzz.textContent = "buzz";
-  // var fizz = fizz.textContent = "fizz";
-  var fizz = "Fizz";
-  var buzz = "Buzz";
-  var fizzBuzz = fizz + buzz;
-
-  if (number % 5 === 0 && number % 3 === 0) {
-    result.textContent = fizzBuzz;
-
-  } else if (number % 5 === 0) {
-    result.textContent = fizz;
-    console.log("Fizz");
-  } else if (number % 3 === 0) {
-    result.textContent = buzz;
-    console.log("Buzz");
-  } else {
-    result.textContent = number;
-    console.log(number);
-  }
-})
